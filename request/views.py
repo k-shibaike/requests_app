bfb36876782abc04386c9c84059dd9a2fd0ee0ac
k-shibaike <info@shibaike-seisakujo.com>
2024-01-16 15:13:50 +0900

    ルーティング終了

--- request/views.py
+++ request/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+
+def index(request):
+  return render(request, 'request/index.html')
+
+def detail(request):
+  if 'post' in request.method.lower():
+        city_number = request.POST.get('location')
+        print("city_number->", city_number)
+
+        # ここにweatherAPIの処理書いてもいい
+
+  return render(request, 'request/detail.html')
